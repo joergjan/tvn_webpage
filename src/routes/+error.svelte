e031967b31d14791793d87aaf1526b18877bf533
Jan Jörg <jan.joerg.jj@gmail.com>
2023-01-30 21:56:01 +0100

    new edit

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,6 +5,11 @@
 
 <div class="h1 flex place-content-center">Seite nicht gefunden :(</div>
 
-<div class="grid grid-cols-3">
-	<img class="col-start-2" src="./images/gif/runnerColor.gif" alt="athlete running gif" />
+<div class="flex justify-center">
+	<div class="px-4 py-2 bg-tvbluelight rounded-lg text-white hover:opacity-90">
+		<a class="text-lg font-semibold" href="/">zurück</a>
+	</div>
+</div>
+<div class="flex justify-center">
+	<img class="h-96" src="./images/gif/runnerColor.gif" alt="athlete running gif" />
 </div>
