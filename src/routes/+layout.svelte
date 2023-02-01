48e749e372fda14d43f6ba9cf80efcb7cc32b7a8
Jan Jörg <jan.joerg.jj@gmail.com>
2023-02-01 09:37:33 +0100

    navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,8 @@
 <script>
-	// @ts-nocheck
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 
 	import '../app.css';
 	import Icon from '@iconify/svelte';
-	import { Hamburger } from 'svelte-hamburgers';
-
-	let open;
-
-	function close() {
-		open = false;
-	}
 
 	const titles = [
 		{ name: 'Unser Verein', href: '/' },
@@ -21,107 +14,99 @@
 	];
 </script>
 
-<div class="py-5 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 font-bold">
-	<div class="text-white bg-tvbluelight">
-		<div class="flex place-content-end">
-			<a href="/">
-				<div class="flex items-center hover:text-gray-400">
-					<div class="h5 pr-5">Turnverein Nussbaumen</div>
-					<div class="">
-						<img class="h-10" src="./images/logos/turner.png" alt="TVN" />
-					</div>
-				</div>
-			</a>
-		</div>
-		<div class="text-white">
-			<div class="absolute top-3">
-				<Hamburger bind:open --color="white" --layer-height="2px" --layer-width="25px" />
-			</div>
-			{#if open}
-				<div class="bg-tvbluelight w-screen pt-10 sm:pt-5">
-					{#each titles as title}
-						<div class="py-3">
-							<a class="hover:text-gray-400" href={title.href} on:click={close}>{title.name} </a>
-						</div>
-					{/each}
-				</div>
-			{/if}
-		</div>
-	</div>
-</div>
-
-<div class="bg-white">
-	<div class="py-5 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-		<slot />
-	</div>
-</div>
-
-<div>
-	<div class="bg-tvbluelight text-white">
-		<nav class="md:flex md:flex-wrap justify-center hidden mt-5">
+<div class="relative">
+	<Navbar
+		navClass="px-2 sm:px-4 py-2.5 absolute w-full z-20 top-0 left-0 border-b"
+		let:hidden
+		let:toggle
+	>
+		<NavBrand href="/">
+			<img src="./images/logos/turner.png" class="mr-3 h-6 sm:h-9" alt="TVN" />
+			<span class="self-center whitespace-nowrap text-xl font-semibold uppercase"
+				>Turnverein Nussbaumen</span
+			>
+		</NavBrand>
+		<NavHamburger on:click={toggle} />
+		<NavUl {hidden}>
 			{#each titles as title}
-				<a class="hover:text-gray-400 px-5" href={title.href}>{title.name}</a>
+				<NavLi href={title.href} class="text-white">{title.name}</NavLi>
 			{/each}
-		</nav>
-		<div class="flex justify-center space-x-6 mt-5 items-center">
-			<a
-				href="https://www.facebook.com/people/TV-Nussbaumen/100064088556190/"
-				target="_blank"
-				rel="noreferrer"
-				class="text-white"
-			>
-				<Icon
-					icon="ant-design:facebook-filled"
-					class="text-white hover:text-gray-400 md:hidden flex"
-					width="20"
-					height="20"
-				/>
-				<Icon
-					icon="ant-design:facebook-filled"
-					class="text-white hover:text-gray-400 hidden md:flex"
-					width="25"
-					height="25"
-				/>
-			</a>
-			<a
-				href="https://www.instagram.com/tvnussbaumen/"
-				target="_blank"
-				rel="noreferrer"
-				class="text-white"
-			>
-				<Icon
-					icon="gg:instagram"
-					class="text-white hover:text-gray-400 md:hidden flex"
-					width="18"
-					height="18"
-				/>
-				<Icon
-					icon="gg:instagram"
-					class="text-white hover:text-gray-400 hidden md:flex"
-					width="23"
-					height="23"
-				/>
-			</a>
-			<a
-				href="https://www.flickr.com/groups/14819815@N20/"
-				target="_blank"
-				rel="noreferrer"
-				class="text-white"
-			>
-				<Icon
-					icon="raphael:flickr"
-					class="text-white hover:text-gray-400 sm:hidden flex"
-					width="20"
-					height="20"
-				/>
-				<Icon
-					icon="raphael:flickr"
-					class="text-white hover:text-gray-400 hidden sm:flex"
-					width="25"
-					height="25"
-				/>
-			</a>
+		</NavUl>
+	</Navbar>
+	<div class="overflow-scroll h-screen sm:pt-12 pt-8">
+		<div class="bg-white">
+			<div class="py-5 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 z-0">
+				<slot />
+			</div>
 		</div>
-		<div class="my-5 text-center">&copy; 2023 Turnverein Nussbaumen.</div>
+		<div>
+			<div class="bg-tvbluelight text-white">
+				<nav class="md:flex md:flex-wrap justify-center hidden mt-5">
+					{#each titles as title}
+						<a class="hover:text-gray-400 px-5" href={title.href}>{title.name}</a>
+					{/each}
+				</nav>
+				<div class="flex justify-center space-x-6 mt-5 items-center">
+					<a
+						href="https://www.facebook.com/people/TV-Nussbaumen/100064088556190/"
+						target="_blank"
+						rel="noreferrer"
+						class="text-white"
+					>
+						<Icon
+							icon="ant-design:facebook-filled"
+							class="text-white hover:text-gray-400 md:hidden flex"
+							width="20"
+							height="20"
+						/>
+						<Icon
+							icon="ant-design:facebook-filled"
+							class="text-white hover:text-gray-400 hidden md:flex"
+							width="25"
+							height="25"
+						/>
+					</a>
+					<a
+						href="https://www.instagram.com/tvnussbaumen/"
+						target="_blank"
+						rel="noreferrer"
+						class="text-white"
+					>
+						<Icon
+							icon="gg:instagram"
+							class="text-white hover:text-gray-400 md:hidden flex"
+							width="18"
+							height="18"
+						/>
+						<Icon
+							icon="gg:instagram"
+							class="text-white hover:text-gray-400 hidden md:flex"
+							width="23"
+							height="23"
+						/>
+					</a>
+					<a
+						href="https://www.flickr.com/groups/14819815@N20/"
+						target="_blank"
+						rel="noreferrer"
+						class="text-white"
+					>
+						<Icon
+							icon="raphael:flickr"
+							class="text-white hover:text-gray-400 sm:hidden flex"
+							width="20"
+							height="20"
+						/>
+						<Icon
+							icon="raphael:flickr"
+							class="text-white hover:text-gray-400 hidden sm:flex"
+							width="25"
+							height="25"
+						/>
+					</a>
+				</div>
+				<div class="my-5 text-center">&copy; 2023 Turnverein Nussbaumen.</div>
+			</div>
+		</div>
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,30 @@
+<script>
+	import { Carousel } from 'flowbite-svelte';
+
+	let images = [
+		{ imgurl: 'https://live.staticflickr.com/65535/52581644661_4701f68e20_c.jpg' },
+		{ imgurl: 'https://live.staticflickr.com/65535/52579943798_bc811955de_c.jpg' }
+	];
+
+	let showThumbs = false;
+	let showCaptions = false;
+</script>
+
 <title>TV Nussbaumen</title>
 
 <div class="flex justify-center overflow-hidden">
-	<img class="py-24 scale-150" src="./images/logos/turner_webpage.png" alt="turner" />
+	<img
+		class="z-10 py-24 scale-150 md:scale-100"
+		src="./images/logos/turner_webpage.png"
+		alt="turner"
+	/>
+</div>
+
+<div class="">
+	<Carousel
+		divClass="overflow-hidden relative h-90 rounded-lg"
+		{images}
+		{showThumbs}
+		{showCaptions}
+	/>
 </div>
