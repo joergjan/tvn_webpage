274a3c99345ef1751971e905df40e355c60fb355
Jan Jörg <jan.joerg.jj@gmail.com>
2023-02-01 13:39:23 +0100

    navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,54 +10,116 @@
 		{ name: 'Downloads', href: '/downloads' },
 		{ name: '404 Test', href: '/error' }
 	];
-
-	let open = false;
-
-	function openMenu() {
-		if (open) {
-			open = false;
-		} else {
-			open = true;
-		}
-	}
-
-	function closeMenu() {
-		open = false;
-	}
 </script>
 
-<div id="img-container" class="absolute left-2 top-2">
-	<img src="./images/logos/turner.png" alt="avatar" class="h-12" />
-</div>
+<nav class="bg-white shadow">
+	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+		<div class="flex h-16 justify-between">
+			<div class="flex">
+				<div class="flex flex-shrink-0 items-center">
+					<img
+						class="block h-8 w-auto lg:hidden"
+						src="./images/logos/turner.png"
+						alt="Turnverein Nussbaumen"
+					/>
+					<img
+						class="hidden h-8 w-auto lg:block"
+						src="./images/logos/turner.png"
+						alt="Turnverein Nussbaumen"
+					/>
+				</div>
+				<div class="hidden md:ml-6 md:flex md:space-x-8">
+					<!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
+					{#each titles as title}
+						<a
+							href={title.href}
+							class="inline-flex items-center border-b-2 hover:border-tvbluelight px-1 pt-1 text-sm font-medium"
+							tabindex="-1"
+							role="menuitem">{title.name}</a
+						>
+					{/each}
+				</div>
+			</div>
 
-<div id="nav-container" class="bg-tvbluelight p-2 flex flex-col items-end">
-	<button class="h-12 w-12 p-2 group" on:click={openMenu}>
-		<svg
-			xmlns="<http://www.w3.org/2000/svg>"
-			viewBox="0 0 20 20"
-			fill="rgb(255, 255, 255)"
-			class="h-full w-full"
-		>
-			<path
-				fill-rule="evenodd"
-				d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-				clip-rule="evenodd"
-			/>
-		</svg>
-	</button>
+			<div class="-mr-2 flex items-center md:hidden">
+				<!-- Mobile menu button -->
+				<button
+					type="button"
+					class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+					aria-controls="mobile-menu"
+					aria-expanded="false"
+				>
+					<span class="sr-only">Open main menu</span>
+					<!--
+			  Icon when menu is closed.
+  
+			  Heroicon name: outline/bars-3
+  
+			  Menu open: "hidden", Menu closed: "block"
+			-->
+					<svg
+						class="block h-6 w-6"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+						/>
+					</svg>
+					<!--
+			  Icon when menu is open.
+  
+			  Heroicon name: outline/x-mark
+  
+			  Menu open: "block", Menu closed: "hidden"
+			-->
+					<svg
+						class="hidden h-6 w-6"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg>
+				</button>
+			</div>
+		</div>
+	</div>
 
-	{#if open}
-		<ul id="nav-menu" class="w-full space-y-2 pr-3 font-semibold text-xl text-right">
+	<!-- Mobile menu, show/hide based on menu state. -->
+	<div class="md:hidden" id="mobile-menu">
+		<div class="space-y-1 pt-2 pb-3">
+			<!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
 			{#each titles as title}
-				<li>
-					<a href={title.href} class="text-white hover:text-gray-400" on:click={closeMenu}
+				<a
+					href={title.href}
+					class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
+					>{title.name}</a
+				>
+			{/each}
+		</div>
+		<div class="border-t border-gray-200 pt-4 pb-3">
+			<div class="mt-3 space-y-1">
+				{#each titles as title}
+					<a
+						href={title.href}
+						class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
 						>{title.name}</a
 					>
-				</li>
-			{/each}
-		</ul>
-	{/if}
-</div>
+				{/each}
+			</div>
+		</div>
+	</div>
+</nav>
 
 <div class="overflow-scroll h-screen">
 	<div class="bg-white">
