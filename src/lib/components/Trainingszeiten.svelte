1203d7b42a97325392c95224d8b70e06c66cc74b
Jan Jörg <jan.joerg.jj@gmail.com>
2023-02-03 15:42:57 +0100

    div

--- src/lib/components/Trainingszeiten.svelte
+++ src/lib/components/Trainingszeiten.svelte
@@ -4,7 +4,7 @@
 
 <div class="grid lg:grid-cols-3 md:grid-cols-2 gap-x-2 gap-y-4">
 	{#each Riegen as riege}
-		<div class="col-box-1 overflow-scroll border-2 rounded-lg">
+		<div class="col-box-1 border-2 rounded-lg">
 			<a href="/riegen">
 				<div class="lg:p-5 md:pd-4 p-2 relative">
 					<h4 class="h4">{riege.name}</h4>
