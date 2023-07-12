4a312de49b8632be4a5172c868b89f4bf431a544
Jan Jörg <jan.joerg.jj@gmail.com>
2023-07-12 09:37:07 +0200

    error redirect

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,22 @@
 <script>
+	let count = 3;
+	const countdown = setInterval(() => {
+		count--;
+		if (count === 0) {
+			clearInterval(countdown);
+			window.location.href = './';
+		}
+	}, 1000);
 </script>
 
 <title>TV Nussbaumen</title>
 
 <div class="h1 flex place-content-center">Seite nicht gefunden :(</div>
 
+<div class="h4 flex place-content-center pt-5">Du wirst weitergeleitet in</div>
+
+<div class="h2 flex place-content-center pt-5">{count}</div>
+
 <div class="flex justify-center">
-	<button type="button" class="button-gray px-4 py-2"> <a href="/">zurück</a></button>
-</div>
-<div class="flex justify-center">
-	<img loading="lazy" class="h-96" src="./images/gif/runnerColor.gif" alt="athlete running gif" />
+	<img loading="lazy" class="h-96" src="../images/gif/runnerColor.gif" alt="athlete running gif" />
 </div>
