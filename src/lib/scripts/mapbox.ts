3b7f6dfe989b783ada50016166b35048b9265a5b
Jan Jörg <jan.joerg.jj@gmail.com>
2023-07-17 10:25:43 +0200

    mapbox

---
+++ src/lib/scripts/mapbox.ts
@@ -0,0 +1,7 @@
+// this script is included in .gitignore
+
+// this api key is not included in .env, because for performance reasons it makes sense to
+// load the mapbox component onMount(). Therefore environment variables cannot be accessed
+
+export const MAPBOX_KEY =
+	'pk.eyJ1Ijoiam9lcmdqYW4iLCJhIjoiY2xqcGhqajNrMjI0MzNkbnVtNGh5dDBvdiJ9.HJ5J6ZUYhBUgmVeHYlIxfA';
