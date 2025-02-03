faffce2a6dbe4a705c3004007b25bc18d2ecad06
Jan Jörg <jan.joerg.jj@gmail.com>
2025-02-03 18:43:22 +0100

    update cli.ts

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -7,7 +7,7 @@
 
 import {defineCliConfig} from 'sanity/cli'
 
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID || '4b45akl7'
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || '<your project ID>'
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
 
 export default defineCliConfig({
