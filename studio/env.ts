ba35bc2935c0546b77b42e90e1f25323a78d8f32
Jan Jörg <jan.joerg.jj@gmail.com>
2025-12-13 21:13:15 +0100

    add app id

--- studio/env.ts
+++ studio/env.ts
@@ -1,2 +1,3 @@
 export const projectId = process.env.SANITY_STUDIO_TVN_PROJECT_ID!
 export const dataset = process.env.SANITY_STUDIO_DATASET!
+export const appId = process.env.SANITY_STUDIO_TVN_APP_ID!
--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -6,7 +6,7 @@
  */
 
 import {defineCliConfig} from 'sanity/cli'
-import {projectId, dataset} from './env'
+import {projectId, dataset, appId} from './env'
 
 export default defineCliConfig({
   api: {
@@ -14,5 +14,5 @@ export default defineCliConfig({
     dataset,
   },
   studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/environment-variables to leanr more about using environment variables for local & production.
-  deployment: {autoUpdates: true},
+  deployment: {autoUpdates: true, appId},
 })
